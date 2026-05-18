f4ca6449d7883d53bdf7a408e490df68ac610374
Kilian <k.coubard2710@gmail.com>
2026-05-18 18:34:55 +0200

    ajout de la commande /vanish

---
+++ src/main/java/fr/kilian/easyAdmin/commands/VanishCommand.java
@@ -0,0 +1,53 @@
+package fr.kilian.easyAdmin.commands;
+
+import fr.kilian.easyAdmin.Main;
+import fr.kilian.easyAdmin.managers.VanishManager;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class VanishCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender,
+                             @NotNull Command command,
+                             @NotNull String label,
+                             @NotNull String[] args) {
+
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("Commande uniquement utilisable en jeu.");
+            return true;
+        }
+
+        if (!player.hasPermission("easyadmin.vanish")) {
+            player.sendMessage(
+                    Component.text("Permission insuffisante.")
+                            .color(NamedTextColor.RED)
+            );
+            return true;
+        }
+
+        VanishManager vanishManager =
+                Main.getInstance().getVanishManager();
+
+        vanishManager.toggle(player);
+
+        if (vanishManager.isVanished(player)) {
+            player.sendMessage(
+                    Component.text("Vous êtes maintenant invisible.")
+                            .color(NamedTextColor.GREEN)
+            );
+        } else {
+            player.sendMessage(
+                    Component.text("Vous êtes maintenant visible.")
+                            .color(NamedTextColor.YELLOW)
+            );
+        }
+
+        return true;
+    }
+}
